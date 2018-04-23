b3a43901fdd2645e4a51235c1307ab31462c611f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WinWebSolution.Module/Order.vb
@@ -0,0 +1,52 @@
+﻿Imports System
+Imports DevExpress.Xpo
+Imports DevExpress.Persistent.BaseImpl
+
+Namespace WinWebSolution.Module
+    Public Class Order
+        Inherits BaseObject
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+        Private fDescription As String
+        Public Property Description() As String
+            Get
+                Return fDescription
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Description", fDescription, value)
+            End Set
+        End Property
+        Private fTotal As Decimal
+        Public Property Total() As Decimal
+            Get
+                Return fTotal
+            End Get
+            Set(ByVal value As Decimal)
+                SetPropertyValue("Total", fTotal, value)
+                If (Not IsLoading) AndAlso (Not IsSaving) AndAlso Product IsNot Nothing Then
+                    Product.UpdateOrdersTotal(True)
+                    Product.UpdateMaximumOrder(True)
+                End If
+            End Set
+        End Property
+        Private fProduct As Product
+        <Association("Product-Orders")> _
+        Public Property Product() As Product
+            Get
+                Return fProduct
+            End Get
+            Set(ByVal value As Product)
+                Dim oldProduct As Product = fProduct
+                SetPropertyValue("Product", fProduct, value)
+                If (Not IsLoading) AndAlso (Not IsSaving) AndAlso oldProduct IsNot fProduct Then
+                    oldProduct = If(oldProduct, fProduct)
+                    oldProduct.UpdateOrdersCount(True)
+                    oldProduct.UpdateOrdersTotal(True)
+                    oldProduct.UpdateMaximumOrder(True)
+                End If
+            End Set
+        End Property
+    End Class
+End Namespace
---
+++ VB/WinWebSolution.Win/Program.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports System.Configuration
+Imports WinWebSolution.Module
+Imports DevExpress.ExpressApp.Security
+
+Namespace WinWebSolution.Win
+    Friend NotInheritable Class Program
+
+        Private Sub New()
+        End Sub
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread> _
+        Shared Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+            Dim winApplication As New WinWebSolutionWindowsFormsApplication()
+            winApplication.ConnectionString = CodeCentralExampleInMemoryDataStoreProvider.ConnectionString
+            If ConfigurationManager.ConnectionStrings("ConnectionString") IsNot Nothing Then
+                winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+            End If
+            Try
+                DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.Register()
+                                winApplication.ConnectionString = DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.ConnectionString
+                winApplication.Setup()
+                winApplication.Start()
+            Catch e As Exception
+                winApplication.HandleException(e)
+            End Try
+        End Sub
+    End Class
+End Namespace
